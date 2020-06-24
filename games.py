458b657dc510d88bd71653075b1317f512c434f7
Eric Tiano <etiano417@gmail.com>
2020-06-24 19:30:21 -0400

    Added best_response function to NFG

--- games.py
+++ games.py
@@ -1,3 +1,5 @@
+import pdb
+
 class NormalFormGame:
     """
     construct a new normal form game
@@ -18,7 +20,7 @@ class NormalFormGame:
         self.rules = rules
 
     """
-    give a list of actions performed by each player, return the given 
+    given a list of actions performed by each player, return the given 
     player's payoff
     """
     def payoff(self, player, actions):
@@ -27,6 +29,40 @@ class NormalFormGame:
         player_payoff = payoffs[player_number]
         return player_payoff
 
+    """
+    given a player and a dictionary mapping the other players in the game to actions, 
+    show the best action for the given player
+    """
+    def best_response(self, responding_player, opponent_actions):
+
+        #create a list of actions chosen by each player
+        chosen_actions = []
+        for player in self.players:
+            if player != responding_player: 
+                chosen_actions.append(opponent_actions[player])
+            else: chosen_actions.append("")        
         
-         
+        pdb.set_trace()
+
+        #find the index of the responding player
+        responding_player_index = self.players.index(responding_player)
+
+        #find the payoff of each of the player's potential actions
+        payoffs = []
+        for action in self.actions[responding_player_index]:
+            chosen_actions[responding_player_index] = action
+            result = self.rules(chosen_actions)
+            payoff = result[responding_player_index]
+            payoffs.append(payoff)
+
+        #find which payoff is highest
+        highest_payoff_index = -999
+        highest_payoff = -10000000000000000
+        for i in range(len(payoffs)):
+            payoff = payoffs[i]
+            if(payoff > highest_payoff):
+                highest_payoff = payoff
+                highest_payoff_index = i
+        highest_payoff_action = self.actions[responding_player_index][highest_payoff_index]
+        return highest_payoff_action
         
